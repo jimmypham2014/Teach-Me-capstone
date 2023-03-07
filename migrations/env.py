33f413d50c232a76143b7ad207ffba45dca89943
jimmy <jimmyfam96@gmail.com>
2023-03-06 21:02:35 -0800

    updated

--- migrations/env.py
+++ migrations/env.py
@@ -1,11 +1,16 @@
-from __future__ import with_statement
+# migrations/env.py file
+
+# existing imports
+from future import with_statement
 
 import logging
 from logging.config import fileConfig
 
-from flask import current_app
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
+
 
 # add new import and environment variable
 import os
